37da85a171563842c0cf2687f65a5e1b154fcb6f
Suman Subedi <ssubedi60@gmail.com>
2025-10-12 19:26:47 -0500

    Created a sql data file for testing

---
+++ spring-jpa-rest-application/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+-- Test data for game_info table
+INSERT INTO game_info (title, genre, platform, price) VALUES 
+('Test Game 1', 'Action', 'PC', 29.99),
+('Test Game 2', 'RPG', 'PlayStation 5', 59.99),
+('Test Game 3', 'Racing', 'Xbox Series X', 49.99);
